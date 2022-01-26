0179bbe679f81cd8bf696778ecb71cbb3cb19943
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2022-01-26 12:36:15 -0300

    feature: dashboard page structure

---
+++ simple-budget/src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+import { SideMenu } from "../../components/SideMenu";
+
+export const Dashboard = () => {
+  return (
+    <Box>
+        <Heading>Dashboard</Heading>
+      <SideMenu />
+    </Box>
+  );
+};
--- simple-budget/src/routes/index.tsx
+++ simple-budget/src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Switch, Route } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
 
 export const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={{}} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 };
