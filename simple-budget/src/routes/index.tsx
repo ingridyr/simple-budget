5d5f67ff80abb84487d59c97a52c64585f727be5
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2022-01-26 16:24:32 -0300

    update: dashboard and SideMenu

--- simple-budget/src/routes/index.tsx
+++ simple-budget/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
-import { Signup } from "../pages/Signup/index";
-import { Login } from "../pages/Login/index";
+import {Signup} from "../pages/Signup/index"
+import {Login} from "../pages/Login/index"
+import { Dashboard } from "../pages/Dashboard";
 
 export const Routes = () => {
   return (
@@ -8,6 +9,7 @@ export const Routes = () => {
       {/* <Route exact path="/" component={} /> */}
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 };
