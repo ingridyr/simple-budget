2c949a1abb82ae75b515c03886cb05e4621ab056
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2022-01-26 12:59:47 -0300

    update: dashboard structure

--- simple-budget/src/pages/Dashboard/index.tsx
+++ simple-budget/src/pages/Dashboard/index.tsx
@@ -1,11 +1,28 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { SideMenu } from "../../components/SideMenu";
 
 export const Dashboard = () => {
   return (
-    <Box>
-        <Heading>Dashboard</Heading>
+    <Flex flexDirection="column" justifyContent="center">
       <SideMenu />
-    </Box>
+      <Heading alignSelf="flex-start" h="50px" m="20px">
+        Expenses by category
+      </Heading>
+      <Flex
+        gap="40px"
+        flexWrap="wrap"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {[1, 2, 3, 4].map((_) => (
+          <Box
+            border="2px solid white"
+            w="500px"
+            h="250px"
+            borderRadius="10px"
+          />
+        ))}
+      </Flex>
+    </Flex>
   );
 };
--- simple-budget/src/routes/index.tsx
+++ simple-budget/src/routes/index.tsx
@@ -1,22 +1,15 @@
 import { Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import { Dashboard } from "../pages/Dashboard";
-=======
 import {Signup} from "../pages/Signup/index"
 import {Login} from "../pages/Login/index"
->>>>>>> a91c87ae71e5311f332f7abf185ef687a1338ec0
+import { Dashboard } from "../pages/Dashboard";
 
 export const Routes = () => {
   return (
     <Switch>
-<<<<<<< HEAD
-      <Route exact path="/" component={{}} />
-      <Route path="/dashboard" component={Dashboard} />
-=======
       {/* <Route exact path="/" component={} /> */}
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
->>>>>>> a91c87ae71e5311f332f7abf185ef687a1338ec0
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 };
