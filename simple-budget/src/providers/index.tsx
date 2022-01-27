5aef43b2839dc86a9bba0e4975e4587b61472b60
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2022-01-27 10:00:34 -0300

    feat: Creating Expenses Provider

--- simple-budget/src/providers/index.tsx
+++ simple-budget/src/providers/index.tsx
@@ -1,11 +1,19 @@
 import { ReactNode } from "react";
 
 import { AuthProvider } from "./AuthContext/index";
+import { BudgetsProvider } from "./BudgetsContext/index";
+import { ExpensesProvider } from "./ExpensesContext/index";
 
 interface ProviderProps {
   children: ReactNode;
 }
 
 export const Providers = ({ children }: ProviderProps) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <BudgetsProvider>
+        <ExpensesProvider>{children}</ExpensesProvider>
+      </BudgetsProvider>
+    </AuthProvider>
+  );
 };
