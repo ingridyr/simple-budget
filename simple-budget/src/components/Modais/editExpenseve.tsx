d36edb2c9a97caf437bf01b5d5214b74b76ce78c
breno santos <breno.angelim@hotmail.com>
2022-01-27 17:49:45 -0300

    style: edit and delete buttons

---
+++ simple-budget/src/components/Modais/editExpenseve.tsx
@@ -0,0 +1,165 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+  useDisclosure,
+  Center,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  titulo: yup.string().required("Titulo obrigatorio"),
+  total: yup.number().required("valor a ser inserido"),
+});
+
+interface ModalData {
+  titulo: string;
+  total: number;
+}
+
+export const ModalEditExpense = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [expenseve, setExpenseve] = useState<ModalData>({} as ModalData);
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<ModalData>({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(expenseve);
+  return (
+    <>
+      <Center w="70px" h="70px">
+        <Button
+          onClick={onOpen}
+          bg="white"
+          color="purple.500"
+          borderRadius="50%"
+          fontSize="40px"
+        >
+          +
+        </Button>
+      </Center>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="sm">
+        <ModalOverlay />
+        <ModalContent
+          bg="black.500"
+          border="1px solid"
+          borderColor="green.500"
+          pb="25px"
+          borderRadius="10px"
+          boxShadow="1px 0px 62px 0px rgb(0,245,155)"
+          as="form"
+          onSubmit={handleSubmit(setExpenseve)}
+        >
+          <ModalHeader pb={4}></ModalHeader>
+          <ModalCloseButton color="purple.500" fontSize="16px" />
+          <ModalBody
+            pb={3.5}
+            w="90%"
+            display="flex"
+            flexDir="column"
+            alignSelf="center"
+          >
+            <FormControl
+              display="flex"
+              flexDir="column"
+              justifyContent="center"
+            >
+              <FormLabel fontSize="20px">Titulo</FormLabel>
+              <ChakraInput
+                bg="white"
+                p="28px 16px"
+                color="black.500"
+                placeholder="Titulo"
+                type="text"
+                {...register("titulo")}
+              />
+              <FormErrorMessage>{errors.titulo?.message}</FormErrorMessage>
+            </FormControl>
+
+            <FormControl
+              mt={4}
+              display="flex"
+              flexDir="column"
+              justifyContent="center"
+            >
+              <FormLabel fontSize="20px">Total</FormLabel>
+              <ChakraInput
+                bg="white"
+                p="28px 16px"
+                color="black.500"
+                placeholder="Total"
+                type="number"
+                {...register("total")}
+              />
+              <FormErrorMessage>{errors.total?.message}</FormErrorMessage>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter
+            w="80%"
+            alignSelf="center"
+            justifyContent="space-around"
+            pr="0px"
+            pl="0px"
+            pb={6}
+          >
+            <Button
+              padding="28px 0px"
+              colorScheme="gray"
+              w="45%"
+              color="black.500"
+              type="submit"
+              border="3px solid"
+              borderColor="white"
+              onClickCapture={() => {}}
+              _hover={{
+                bg: "gray.600",
+                border: "3px solid",
+                borderColor: "purple.500",
+                color: "white",
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              padding="28px 0px"
+              colorScheme="gray"
+              w="45%"
+              color="black.500"
+              border="3px solid"
+              borderColor="white"
+              onClickCapture={() => {}}
+              _hover={{
+                bg: "gray.600",
+                border: "3px solid",
+                borderColor: "purple.500",
+                color: "white",
+              }}
+            >
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
