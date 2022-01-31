9a1b97ef8c98df35903b4a9cae4a4ce545a96442
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2022-01-31 19:59:36 -0300

    fix: redirect to home if not authenticate

--- simple-budget/src/routes/index.tsx
+++ simple-budget/src/routes/index.tsx
@@ -3,15 +3,21 @@ import { Home } from "../pages/Home/index";
 import { Signup } from "../pages/Signup/index";
 import { Login } from "../pages/Login/index";
 import { Dashboard } from "../pages/Dashboard/index";
-// import { useAuth } from "../providers/AuthContext";
+import { useAuth } from "../providers/AuthContext";
+import { Redirect } from "react-router-dom";
 
 export const Routes = () => {
+
+  const {accessToken} = useAuth()
+
   return (
     <Switch>
       <Route exact path="/" component={Home}/> 
       <Route path="/signup" component={Signup}/>
       <Route path="/login" component={Login}/>
-      <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/dashboard" component={Dashboard}>
+        {!accessToken && <Redirect to="/"/>}
+      </Route>
     </Switch>
   );
 };
