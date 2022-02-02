8fd50eee276192f3a745c3a3088b5b32520dc532
André Martins <andresmartins910@gmail.com>
2022-02-02 11:04:02 -0300

    styles: improved view expenses modal and cards

--- simple-budget/src/components/Modais/viewExpenses.tsx
+++ simple-budget/src/components/Modais/viewExpenses.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Flex,
   Heading,
+  Image,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -16,7 +17,7 @@ import { FaTrash } from "react-icons/fa";
 import { useAuth } from "../../providers/AuthContext";
 import { useExpenses } from "../../providers/ExpensesContext";
 import { useDisclosure } from "@chakra-ui/react";
-
+import EmptyStreet from "../../assets/Empty.svg";
 import { ModalEditExpense } from "../Modais/editExpenseve";
 
 import { useState } from "react";
@@ -78,13 +79,23 @@ export const ModalViewExpenses = ({
         <ModalOverlay />
         <ModalContent
           bgColor="black.500"
-          boxShadow="0px 1px 7px 2px #00F59B"
+          boxShadow="0px 1px 6px 1px #00F59B"
           marginY="auto"
         >
           <ModalHeader>
             <Flex alignItems="center" justifyContent="space-between">
-              <Heading>{budgetName}</Heading>
-              <ModalCloseButton m="10px" color="purple.500" fontSize="16px" />
+              <Heading as="h1" fontSize={["2xl", "3xl"]} fontWeight="normal">
+                {budgetName}
+              </Heading>
+              <ModalCloseButton
+                mt="10px"
+                color="purple.500"
+                fontSize="16px"
+                _hover={{
+                  transition: "0.2s",
+                  color: "green.500",
+                }}
+              />
             </Flex>
           </ModalHeader>
           <ModalBody
@@ -107,67 +118,89 @@ export const ModalViewExpenses = ({
               },
             }}
           >
-            {expenses.map((item) => {
-              return (
-                <>
-                  <Flex
-                    bgColor="black.300"
-                    borderRadius="5px"
-                    gap="10px"
-                    m="10px 0"
-                    p="10px 15px"
-                    justifyContent="space-between"
-                  >
-                    <Flex flexDirection="column">
-                      <Flex
-                        fontSize="16px"
-                        gap="10px"
-                        width="200px"
-                        justifyContent="space-between"
-                      >
-                        <Heading color="white.0" size="md">
-                          {item.name}
-                        </Heading>
-                        <Heading color="gray.350" size="md">
-                          {item.type}
-                        </Heading>
+            {expenses.length > 0 ? (
+              expenses.map((item) => {
+                return (
+                  <>
+                    <Flex
+                      bgColor="black.300"
+                      borderRadius="5px"
+                      // gap="10px"
+                      mb="4"
+                      pt="3"
+                      pl="3"
+                      pr="3"
+                      justifyContent="space-between"
+                    >
+                      <Flex flexDirection="column">
+                        <Flex
+                          fontSize="16px"
+                          gap="10px"
+                          width="200px"
+                          justifyContent="space-between"
+                        >
+                          <Heading
+                            color="white.0"
+                            size="md"
+                            fontWeight="normal"
+                          >
+                            {item.name}
+                          </Heading>
+                          <Heading
+                            color="gray.300"
+                            size="md"
+                            fontWeight="normal"
+                          >
+                            {item.type}
+                          </Heading>
+                        </Flex>
+                        <Text
+                          fontSize="sm"
+                          color="gray.300"
+                          // marginLeft="5px"
+                          // p="10px"
+                        >
+                          {item.description}
+                        </Text>
                       </Flex>
-                      <Text
-                        fontSize="sm"
-                        color="white.0"
-                        marginLeft="5px"
-                        p="10px"
-                      >
-                        {item.description}
-                      </Text>
+                      <Box display="block">
+                        <Text
+                          fontFamily="other"
+                          color="green.500"
+                          fontWeight="500"
+                        >
+                          R$ {item.amount.toFixed(2)}
+                        </Text>
+                        <Flex
+                          m="10px"
+                          alignItems="center"
+                          gap="10px"
+                          color="gray.300"
+                        >
+                          <AiFillEdit
+                            size={25}
+                            cursor="pointer"
+                            onClick={() => modalSelect(item)}
+                          />
+                          <FaTrash
+                            size={19}
+                            cursor="pointer"
+                            onClick={() => handleDelete(item.id, accessToken)}
+                          />
+                        </Flex>
+                      </Box>
                     </Flex>
-                    <Box display="block">
-                      <Text
-                        fontFamily="other"
-                        color="green.500"
-                        fontWeight="500"
-                      >
-                        R$ {item.amount.toFixed(2)}
-                      </Text>
-                      <Flex m="10px" alignItems="center" gap="10px">
-                        <AiFillEdit
-                          color="gray.300"
-                          size={25}
-                          cursor="pointer"
-                          onClick={() => modalSelect(item)}
-                        />
-                        <FaTrash
-                          color="gray.300"
-                          size={18}
-                          cursor="pointer"
-                          onClick={() => handleDelete(item.id, accessToken)}
-                        />
-                      </Flex>
-                    </Box>
-                  </Flex>
-                </>
-              );
-            })}
+                  </>
+                );
+              })
+            ) : (
+              <>
+                <Text color="green.500" fontSize={["lg", "xl"]} w="100%" pb="6">
+                  You don't have any registered expense.
+                </Text>
+                <Image src={EmptyStreet} alt="Empty" pb="6" />
+              </>
+            )}
             <ModalEditExpense
               isOpen={isModalEditExpenseOpen}
               onOpen={onModalEditExpenseOpen}
