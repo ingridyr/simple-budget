8d8af3bf7080a4e52d4f823bdb6585b53c7a23e4
Gabriel <gabriel.such@hotmail.com>
2022-02-03 16:29:36 -0300

    fix: cypress fix

--- simple-budget/cypress/integration/page.spec.js
+++ simple-budget/cypress/integration/page.spec.js
@@ -3,7 +3,8 @@ context("Enters Home Page", () => {
         cy.visit("http://localhost:3000")
         cy.viewport(1440, 900)
 
-        cy.get(".css-ke8vmv > :nth-child(2)").click()
+        cy.get(".css-16kwn5d").click()
+        cy.wait(1000)
     })
 })
 
@@ -15,6 +16,7 @@ context("Create new Register", () => {
         cy.get(":nth-child(7) > #field-1").type("123456")
         cy.get(":nth-child(9) > #field-1").type("123456")
         cy.get(".chakra-button").click()
+        cy.wait(1000)
     })
 })
 
@@ -23,7 +25,8 @@ context("Login", () => {
         cy.visit("http://localhost:3000/login")
         cy.get(":nth-child(3) > #field-1").type("teste@hotmail.com")
         cy.get(":nth-child(5) > #field-1").type("123456")
-        cy.get(".css-b80e06").click()
+        cy.get("button[type=submit]").click()
+        cy.wait(1000)
     })
 })
 
@@ -33,20 +36,23 @@ context("Enters Dashboard Page" , () => {
         cy.get("#field-8").type("Escola")
         cy.get("#field-9").type("2400")
         cy.get("button[type=submit]").click()
+        cy.wait(1000)
     })
 
     it("Should be able to Add new Expense", () => {
         cy.contains("Add expense").click()
-        cy.get("select").select("food")
+        cy.get("select").select("Food")
         cy.get(":nth-child(3) > #field-17").type("Alimentação")
         cy.get(":nth-child(5) > #field-17").type("Despesa destinado à comida da criança")
         cy.get(":nth-child(7) > #field-17").type("350")
         cy.get("button[type=submit]").click()
+        cy.wait(1000)
     })
 
     it("Should be able to View Expenses", () => {
         cy.contains("View expenses").click()
-        cy.get('.css-aumf0v > [viewBox="0 0 1024 1024"]').click()
+        cy.get('.css-zw1ept > [viewBox="0 0 1024 1024"] > path').click()
+        cy.wait(1000)
     })
 
     it("Should be able to Edit Expense", () => {
@@ -56,18 +62,22 @@ context("Enters Dashboard Page" , () => {
         cy.get("#field-21").clear().type("600")
         cy.get("button[type=submit]").click()
         cy.get("#chakra-modal-18 > .chakra-modal__close-btn > .chakra-icon > path").click()
+        cy.wait(1000)
     })
 
     it("Should be able to Delete Expense", () => {
-        cy.get('.css-aumf0v > [viewBox="0 0 448 512"] > path').click()
+        cy.get('.css-zw1ept > [viewBox="0 0 448 512"] > path').click()
         cy.get(".chakra-modal__close-btn").click()
+        cy.wait(1000)
     })
 
     it("Should be able to Delete Budget", () => {
         cy.get(".css-7g5sjm > path").click()
+        cy.wait(1000)
     })
 
     it("Should be able to Logout", () => {
         cy.get(":nth-child(4) > .css-tvs47z > svg").click()
+        cy.wait(1000)
     })
 })
