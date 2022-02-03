bc35af382f6d0b2b1a45b64a98571696b65f47f4
Gabriel <gabriel.such@hotmail.com>
2022-02-03 19:10:56 -0300

    fix: cypress bug fixes

--- simple-budget/cypress/integration/page.spec.js
+++ simple-budget/cypress/integration/page.spec.js
@@ -11,7 +11,7 @@ context("Enters Home Page", () => {
 context("Create new Register", () => {
     it("Should be able to create a new Register", () => {
         cy.visit("http://localhost:3000/signup")
-        cy.get(":nth-child(3) > #field-1").type("Cypress Teste")
+        cy.get(":nth-child(3) > #field-1").type("Teste")
         cy.get(":nth-child(5) > #field-1").type("teste@hotmail.com")
         cy.get(":nth-child(7) > #field-1").type("123456")
         cy.get(":nth-child(9) > #field-1").type("123456")
@@ -51,22 +51,20 @@ context("Enters Dashboard Page" , () => {
 
     it("Should be able to View Expenses", () => {
         cy.contains("View expenses").click()
-        cy.get('.css-zw1ept > [viewBox="0 0 1024 1024"] > path').click()
+        cy.get('.css-aumf0v > [viewBox="0 0 1024 1024"]').click()
         cy.wait(1000)
     })
 
     it("Should be able to Edit Expense", () => {
-        cy.wait(1000)
-        cy.get("#field-19").clear().type("Alimentação Mensal")
-        cy.get("#field-20").clear().type("Despesa destinado à comida mensal na escola")
-        cy.get("#field-21").clear().type("600")
+        cy.get(':nth-child(3) > .chakra-input').clear().type("Alimentação Mensal")
+        cy.get(':nth-child(5) > .chakra-input').clear().type("Despesa destinado à comida mensal na escola")
+        cy.get(':nth-child(7) > .chakra-input').clear().type("600")
         cy.get("button[type=submit]").click()
-        cy.get("#chakra-modal-18 > .chakra-modal__close-btn > .chakra-icon > path").click()
         cy.wait(1000)
     })
 
     it("Should be able to Delete Expense", () => {
-        cy.get('.css-zw1ept > [viewBox="0 0 448 512"] > path').click()
+        cy.get('.css-aumf0v > [viewBox="0 0 448 512"] > path').click()
         cy.get(".chakra-modal__close-btn").click()
         cy.wait(1000)
     })
